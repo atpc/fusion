b0864412e4059c34b56f2a5006dfb11e4261c81a
Thomas Orlando <thomas.orlando@atpc.one>
2019-01-21 12:21:19 +1300

    Add component definitions

---
+++ src/main/kotlin/one/atpc/fusion/ui/components.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2019  Thomas Orlando, ATPC
+ *
+ * This file is part of Fusion.
+ *
+ * Fusion is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Fusion is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Fusion.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package one.atpc.fusion.ui
+
+import org.jdesktop.swingx.JXButton
+import org.jdesktop.swingx.JXLabel
+import javax.swing.JComponent
+import javax.swing.JPanel
+
+typealias XComponent = JComponent
+
+// Widget components
+typealias XText = JXLabel
+typealias XButton = JXButton
+
+// Containers
+typealias XPanel = JPanel
