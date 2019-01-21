21a9e678c46dc4073ba235d4d9da1ab7fcc1b305
Thomas Orlando <thomas.orlando@atpc.one>
2019-01-21 14:08:30 +1300

    Add Number extensions
    Number.isFloatingPoint determines wether a number is a floating point
    (instance of Float, Double or BigDecimal).

---
+++ src/main/kotlin/one/atpc/fusion/number-extensions.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2019  Thomas Orlando, ATPC
+ *
+ * This file is part of Fusion.
+ *
+ * Fusion is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Fusion is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Fusion.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package one.atpc.fusion
+
+import org.jetbrains.annotations.Contract
+import java.math.BigDecimal
+
+/**
+ * Determines whether a number is a floating-point.
+ *
+ * @return `true` if the number is a floating-point number,
+ *          `false` otherwise.
+ */
+val Number.isFloatingPoint
+    @Contract(pure = true)
+    get() = this is Float || this is Double || this is BigDecimal
