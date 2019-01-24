071bd3556fe9bd12dd9e085597fac3d01adba95d
Thomas Orlando <thomas.orlando@atpc.one>
2019-01-25 00:28:55 +1300

    Add XWindow type
    Implements XScreenPlaceable and XContainer. Otherwise just acts like
    a normal JWindow.

---
+++ src/main/kotlin/one/atpc/fusion/ui/XWindow.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2019  Thomas Orlando, ATPC
+ *
+ * This file is part of Fusion.
+ *
+ * Fusion is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Fusion is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Fusion.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package one.atpc.fusion.ui
+
+import java.awt.Frame
+import java.awt.GraphicsConfiguration
+import java.awt.Window
+import javax.swing.JWindow
+
+open class XWindow : JWindow, XScreenPlaceable, XContainer, XContainer.SwingImpl {
+
+    constructor() : super()
+
+    constructor(gc: GraphicsConfiguration) : super(gc)
+
+    constructor(owner: Frame) : super(owner)
+
+    constructor(owner: Window) : super(owner)
+
+    constructor(owner: Window, gc: GraphicsConfiguration) : super(owner, gc)
+
+
+    override fun draw(g: XGraphics)
+            = throw XException(UnsupportedOperationException("Method draw(XGraphics) is not supported in XWindow!"))
+
+
+    override fun setLocationToCenter() = this.setLocationRelativeTo(null)
+
+}
