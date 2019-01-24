25aea86f957d9215ad59f38125acdf450f5859a8
Thomas Orlando <thomas.orlando@atpc.one>
2019-01-24 13:00:03 +1300

    Add new Color type
    The Color type is a more modern version of the AWT Color type with
    new constructor types like Color.rgb(), Color.hsva() and new methods
    like deriveAlpha() to derive new colors with a different alpha value.
    
    The XGraphics type now has to use java.awt.Color explicitly.

---
+++ src/main/kotlin/one/atpc/fusion/ui/Color.kt
@@ -0,0 +1,148 @@
+/*
+ * Copyright (c) 2019  Thomas Orlando, ATPC
+ *
+ * This file is part of Fusion.
+ *
+ * Fusion is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Fusion is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Fusion.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package one.atpc.fusion.ui
+
+import java.awt.color.ColorSpace
+
+open class Color : java.awt.Color {
+    // TODO Use genuine algorithms to construct colors from Double instead of float conversion
+    companion object {
+
+        // New constructor: Use the unsigned byte values directly, thanks to Kotlin unsigned types
+        @JvmStatic
+        fun rgb(r: UByte, g: UByte, b: UByte): one.atpc.fusion.ui.Color = Color.rgb(r.toInt(), g.toInt(), b.toInt())
+
+        @JvmStatic
+        fun rgb(r: Int, g: Int, b: Int): one.atpc.fusion.ui.Color = one.atpc.fusion.ui.Color(r, g, b)
+
+        @JvmStatic
+        fun rgb(r: Float, g: Float, b: Float): one.atpc.fusion.ui.Color = one.atpc.fusion.ui.Color(r, g, b)
+
+        @JvmStatic
+        fun rgb(r: Double, g: Double, b: Double): one.atpc.fusion.ui.Color =
+            one.atpc.fusion.ui.Color(r.toFloat(), g.toFloat(), b.toFloat())
+
+        @JvmStatic
+        fun rgb(rgb: Int): one.atpc.fusion.ui.Color = one.atpc.fusion.ui.Color(rgb)
+
+        @JvmStatic
+        fun rgb(rgb: UInt): one.atpc.fusion.ui.Color = one.atpc.fusion.ui.Color(rgb.toInt())
+
+
+        @JvmStatic
+        fun rgba(r: UByte, g: UByte, b: UByte, a: UByte) = Color.rgba(r.toInt(), g.toInt(), b.toInt(), a.toInt())
+
+        @JvmStatic
+        fun rgba(r: Int, g: Int, b: Int, a: Int): one.atpc.fusion.ui.Color = one.atpc.fusion.ui.Color(r, g, b, a)
+
+        @JvmStatic
+        fun rgba(r: Float, g: Float, b: Float, a: Float): one.atpc.fusion.ui.Color = one.atpc.fusion.ui.Color(r, g, b, a)
+
+        @JvmStatic
+        fun rgba(r: Double, g: Double, b: Double, a: Double): one.atpc.fusion.ui.Color =
+            one.atpc.fusion.ui.Color(r.toFloat(), g.toFloat(), b.toFloat(), a.toFloat())
+
+        @JvmStatic
+        fun rgba(rgba: UInt): one.atpc.fusion.ui.Color = Color(rgba.toInt(), hasAlpha = true)
+
+
+        /**
+         * Creates a `Color` object based on the specified values
+         * for the HSB color model.
+         * <br>
+         * The `s` and `v` components should be
+         * floating-point values between zero and one
+         * (numbers in the range `0..1`).  The `h` component
+         * can be any floating-point number.  The floor of this number is
+         * subtracted from it to create a fraction between `0` and `1`.  This
+         * fractional number is then multiplied by `360` to produce the hue
+         * angle in the HSV color model.
+         *
+         * @param h The hue component.
+         * @param s The saturation of the color (`0..1`).
+         * @param v The value (brightness) of the color (`0..1`).
+         * @return  A `Color` object with the specified hue,
+         *                                 saturation, and value.
+         */
+        @JvmStatic
+        fun hsv(h: Float, s: Float, v: Float): one.atpc.fusion.ui.Color = Color.rgb(HSBtoRGB(h, s, v))
+
+        @JvmStatic
+        fun hsv(h: Double, s: Double, v: Double): one.atpc.fusion.ui.Color = hsv(h.toFloat(), s.toFloat(), v.toFloat())
+
+
+        @JvmStatic
+        fun hsva(h: Float, s: Float, v: Float, a: Float): one.atpc.fusion.ui.Color {
+            if (a < 0.0f || a > 1.0f)
+                throw IllegalArgumentException("Color parameter outside of expected range: Alpha")
+
+            // TODO FIX shifting
+            return Color.rgba(
+                (((a * 255 + 0.5).toUInt() shl 24) or (HSBtoRGB(h, s, v).toUInt() and 0x00_FF_FF_FFU))
+            )
+        }
+
+        @JvmStatic
+        fun hsva(h: Double, s: Double, v: Double, a: Double): one.atpc.fusion.ui.Color = this.hsva(h.toFloat(), s.toFloat(), v.toFloat(), a.toFloat())
+
+    }
+
+    protected constructor(r: Int, g: Int, b: Int) : super(r, g, b)
+
+    protected constructor(r: Int, g: Int, b: Int, a: Int) : super(r, g, b, a)
+
+    protected constructor(rgb: Int) : super(rgb)
+
+    protected constructor(rgba: Int, hasAlpha: Boolean) : super(rgba, hasAlpha)
+
+    protected constructor(r: Float, g: Float, b: Float) : super(r, g, b)
+
+    protected constructor(r: Float, g: Float, b: Float, a: Float) : super(r, g, b, a)
+
+    constructor(cspace: ColorSpace, components: FloatArray, alpha: Float) : super(cspace, components, alpha)
+
+
+    fun getRGBComponents(): FloatArray = this.getRGBComponents(null)
+
+    fun getRGBColorComponents(): FloatArray = this.getRGBColorComponents(null)
+
+    fun deriveAlpha(alpha: Float): one.atpc.fusion.ui.Color {
+        if (alpha.isNaN())
+            throw IllegalArgumentException("Illegal alpha value: $alpha!")
+
+        var rgb = FloatArray(3)
+        rgb = this.getRGBColorComponents(rgb)
+        return Color.rgba(rgb[0], rgb[1], rgb[2], alpha)
+    }
+
+    fun deriveAlpha(alpha: Double): one.atpc.fusion.ui.Color = this.deriveAlpha(alpha.toFloat())
+
+    fun deriveAlpha(alpha: Int): one.atpc.fusion.ui.Color = one.atpc.fusion.ui.Color(red, green, blue, alpha)
+
+    fun deriveAlpha(alpha: UByte): one.atpc.fusion.ui.Color = this.deriveAlpha(alpha.toInt())
+
+
+    override fun toString(): String
+            = "${javaClass.typeName}[r=$red,g=$green,b=$blue,a=$alpha]"
+
+}
+
+
+private fun Double.toUInt() = this.toLong().toUInt()
--- src/main/kotlin/one/atpc/fusion/ui/XGraphics.kt
+++ src/main/kotlin/one/atpc/fusion/ui/XGraphics.kt
@@ -92,7 +92,7 @@ open class XGraphics internal constructor(private val g2d: Graphics2D) : Graphic
 
     override fun getDeviceConfiguration(): GraphicsConfiguration = g2d.deviceConfiguration
 
-    override fun getBackground(): Color = g2d.background
+    override fun getBackground(): java.awt.Color = g2d.background
 
     override fun clip(s: Shape?) = g2d.clip(s)
 
@@ -121,7 +121,7 @@ open class XGraphics internal constructor(private val g2d: Graphics2D) : Graphic
 
     override fun setPaintMode() = g2d.setPaintMode()
 
-    override fun getColor(): Color = g2d.color
+    override fun getColor(): java.awt.Color = g2d.color
 
     override fun scale(sx: PxDouble, sy: PxDouble) = g2d.scale(sx, sy)
 
@@ -137,7 +137,7 @@ open class XGraphics internal constructor(private val g2d: Graphics2D) : Graphic
     override fun drawImage(img: Image?, x: Px, y: Px, width: Px, height: Px, observer: ImageObserver?): Boolean
             = g2d.drawImage(img, x, y, width, height, observer)
 
-    override fun drawImage(img: Image?, x: Px, y: Px, bgcolor: Color?, observer: ImageObserver?): Boolean
+    override fun drawImage(img: Image?, x: Px, y: Px, bgcolor: java.awt.Color?, observer: ImageObserver?): Boolean
             = g2d.drawImage(img, x, y, bgcolor, observer)
 
     override fun drawImage(
@@ -146,7 +146,7 @@ open class XGraphics internal constructor(private val g2d: Graphics2D) : Graphic
         y: Px,
         width: Px,
         height: Px,
-        bgcolor: Color?,
+        bgcolor: java.awt.Color?,
         observer: ImageObserver?
     ): Boolean
             = g2d.drawImage(img, x, y, width, height, bgcolor, observer)
@@ -175,7 +175,7 @@ open class XGraphics internal constructor(private val g2d: Graphics2D) : Graphic
         sy1: Int,
         sx2: Int,
         sy2: Int,
-        bgcolor: Color?,
+        bgcolor: java.awt.Color?,
         observer: ImageObserver?
     ): Boolean
             = g2d.drawImage(img, dx1, dy1, dx2, dy2, sx1, sy1, sx2, sy2, bgcolor, observer)
@@ -183,7 +183,7 @@ open class XGraphics internal constructor(private val g2d: Graphics2D) : Graphic
     override fun getFontRenderContext(): FontRenderContext
             = g2d.fontRenderContext
 
-    override fun setXORMode(c1: Color?)
+    override fun setXORMode(c1: java.awt.Color?)
             = g2d.setXORMode(c1)
 
     override fun addRenderingHints(hints: MutableMap<*, *>?)
@@ -273,7 +273,7 @@ open class XGraphics internal constructor(private val g2d: Graphics2D) : Graphic
     override fun fillPolygon(xPoints: IntArray?, yPoints: IntArray?, nPoints: Int)
             = g2d.fillPolygon(xPoints, yPoints, nPoints)
 
-    override fun setColor(c: Color?)
+    override fun setColor(c: java.awt.Color?)
             = g2d.setColor(c)
 
     override fun setRenderingHint(hintKey: RenderingHints.Key?, hintValue: Any?)
@@ -291,7 +291,7 @@ open class XGraphics internal constructor(private val g2d: Graphics2D) : Graphic
     override fun hit(rect: Rectangle?, s: Shape?, onStroke: Boolean): Boolean
             = g2d.hit(rect, s, onStroke)
 
-    override fun setBackground(color: Color?)
+    override fun setBackground(color: java.awt.Color?)
             = g2d.setBackground(color)
 
 }
