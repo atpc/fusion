3024d8cff706e00d233019efdf6e812cf82ebec4
Thomas Orlando <thomas.orlando@atpc.one>
2019-01-24 17:33:14 +1300

    Add isWindowTranslucencySupported() function
    The function is a replacement for determing the translucency support
    of the default screen device.

---
+++ src/main/kotlin/one/atpc/fusion/ui/isWindowTranslucencySupported.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2019  Thomas Orlando, ATPC
+ *
+ * This file is part of Fusion.
+ *
+ * Fusion is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Fusion is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Fusion.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package one.atpc.fusion.ui
+
+import java.awt.GraphicsEnvironment
+
+
+typealias WindowTranslucency = java.awt.GraphicsDevice.WindowTranslucency
+
+// TODO Add XWindow.setOpacity(), XFrame and XDialog to @see
+/**
+ * Determines whether the systems has the specified window [translucency] capabilities.
+ * It is best practice to test the systems support for window translucency before
+ * attempting to set a window's opacity.
+ * <br>
+ * **See also:** [Oracle Docs > How to Create Translucent and Shaped Windows](https://docs.oracle.com/javase/tutorial/uiswing/misc/trans_shaped_windows.html)
+ *
+ * @param translucency The translucency capabilities to test for.
+ *                     Either `PERPIXEL_TRANSPARENT`, `TRANSLUCENT` or `PERPIXEL_TRANSLUCENT`.
+ * @return `true`, if the systems supports the specified translucency capabilities, `false` otherwise.
+ * @author Thomas Orlando
+ */
+fun isWindowTranslucencySupported(translucency: WindowTranslucency): Boolean
+        = GraphicsEnvironment.getLocalGraphicsEnvironment()
+                .defaultScreenDevice.isWindowTranslucencySupported(translucency)
