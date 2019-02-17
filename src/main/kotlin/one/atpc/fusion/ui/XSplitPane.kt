06ba5be980eff443bc7ba05be584f69305ff5011
Thomas Orlando <thomas.orlando@atpc.one>
2019-02-17 19:27:34 +1300

    Add XSplitPane

---
+++ src/main/kotlin/one/atpc/fusion/ui/XSplitPane.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2019  Thomas Orlando, ATPC
+ *
+ * This file is part of Fusion.
+ *
+ * Fusion is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Fusion is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Fusion.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package one.atpc.fusion.ui
+
+import org.intellij.lang.annotations.MagicConstant
+import java.awt.Component
+import java.awt.Graphics
+import javax.swing.JSplitPane
+
+open class XSplitPane : JSplitPane, XView, XView.SwingImpl {
+
+    constructor() : super()
+
+    constructor(@MagicConstant(intValues = [HORIZONTAL_SPLIT.toLong(), VERTICAL_SPLIT.toLong()]) newOrientation: Int)
+            : super(newOrientation)
+
+    constructor(@MagicConstant(intValues = [HORIZONTAL_SPLIT.toLong(), VERTICAL_SPLIT.toLong()]) newOrientation: Int,
+                newContinuousLayout: Boolean) : super(newOrientation, newContinuousLayout)
+
+    constructor(@MagicConstant(intValues = [HORIZONTAL_SPLIT.toLong(), VERTICAL_SPLIT.toLong()]) newOrientation: Int,
+                newLeftComponent: Component,
+                newRightComponent: Component)
+            : super(newOrientation, newLeftComponent, newRightComponent)
+
+    constructor(@MagicConstant(intValues = [HORIZONTAL_SPLIT.toLong(), VERTICAL_SPLIT.toLong()]) newOrientation: Int,
+                newContinuousLayout: Boolean,
+                newLeftComponent: Component,
+                newRightComponent: Component)
+            : super(newOrientation, newContinuousLayout, newLeftComponent, newRightComponent)
+
+
+    override fun draw(g: XGraphics) = super.paintComponent(g)
+
+    final override fun paintComponent(g: Graphics?) = XView.SwingImpl.paintComponent(this, g)
+
+
+    companion object {
+        const val HORIZONTAL_SPLIT = JSplitPane.HORIZONTAL_SPLIT
+        const val VERTICAL_SPLIT = JSplitPane.VERTICAL_SPLIT
+    }
+
+}
