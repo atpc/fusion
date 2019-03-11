064d9486e0e9b2c0a6faee3594fb21eea490c830
Thomas Orlando <thomas.orlando@atpc.one>
2019-03-12 10:10:24 +1300

    Add TextPopupFeature
    The feature shows a popup menu with actions to cut, copy, paste, delete
    and select all text, enabling and disabling menu items contextually
    (depending on selected text and the type of the component,
    e.g. password fields will not have cut and copy option).
    The menu does not show the key shortcuts deliberately, since they are
    known to virtually every user, and would create unnecessary clutter
    while conveying only unnecessary and superfluous information.
    
    The model for this feature was the URL field popup menu in Firefox 65.

---
+++ src/demo/kotlin/one/atpc/fusion/ui/feature/TextPopupFeatureDemo.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2019  Thomas Orlando, ATPC
+ *
+ * This file is part of Fusion.
+ *
+ * Fusion is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Fusion is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Fusion.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package one.atpc.fusion.ui.feature
+
+object TextPopupFeatureDemo {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val result = DemoUtils.makeTextFieldFrame(this::class.simpleName ?: "TextPopupFeatureDemo")
+        result.second.addFeature(TextPopupFeature(result.second))
+        result.first.isVisible = true
+    }
+
+}
