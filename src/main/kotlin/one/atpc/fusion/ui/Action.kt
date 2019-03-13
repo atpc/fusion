8eae1e76708c2df64cf29cfd71ee6d1796cdff08
Thomas Orlando <thomas.orlando@atpc.one>
2019-03-13 22:50:43 +1300

    Remove obsolete TODO comment in Action.kt

--- src/main/kotlin/one/atpc/fusion/ui/Action.kt
+++ src/main/kotlin/one/atpc/fusion/ui/Action.kt
@@ -28,8 +28,6 @@ operator fun <T> Action.get(key: String): T?
 operator fun <T> Action.set(key: String, value: T?)
         = this.putValue(key, value)
 
-// TODO Document throws NullPointerException
-
 var Action.name: String?
     get() = this[Action.NAME]
     set(value) {
