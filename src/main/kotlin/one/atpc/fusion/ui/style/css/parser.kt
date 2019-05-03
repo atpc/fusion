e4f34ff39ee06b6c52cf197a77db292480153234
Thomas Orlando <thomas.orlando@atpc.one>
2019-05-03 18:38:46 +1200

    Reorganize parser code
    Separate tokens from other lists by putting them in the
    inline class Tokens.
    The other type of List<String> is encapsuled in the typealias Line.

--- src/main/kotlin/one/atpc/fusion/ui/style/css/parser.kt
+++ src/main/kotlin/one/atpc/fusion/ui/style/css/parser.kt
@@ -23,46 +23,9 @@ import one.atpc.fusion.util.get
 import one.atpc.fusion.util.split
 
 internal fun parse(tokens: List<String>): String {
-    val blocks = splitToBlocks(cleanTokens(tokens))
+    val blocks = Tokens(tokens).clean().splitToBlocks()
     return "$tokens\n\n\n$blocks"
     // TODO Build a selector dependency graph and apply the style graph along
-}
-
-
-private fun cleanTokens(tokens: List<String>): List<String> = tokens.filter { t -> t.isNotBlank() }
-
-
-private data class StyleBlock(val selectors: List<String>,
-                              val declarations: List<StyleDeclaration>) {
-
-    override fun toString(): String {
-        val builder = StringBuilder("$selectors {\n")
-        declarations.forEach { declaration ->
-            builder.append("\t$declaration")
-        }
-        builder.append('}')
-
-        return builder.toString()
-    }
-
-    companion object {
-
-        fun fromTokens(tokens: List<String>): StyleBlock {
-            val blockStartIndex = tokens.indexOf(BLOCK_START)
-            val blockEndIndex = tokens.lastIndexOf(BLOCK_END)
-
-            val selectors = tokens.subList(0, blockStartIndex)
-            val lines = splitToLines(tokens.subList(blockStartIndex+1, blockEndIndex))
-
-            return StyleBlock(selectors, lines.map(::toDeclaration))
-        }
-
-    }
-
-}
-
-private data class StyleDeclaration(val property: List<String>, val value: List<String>) {
-    override fun toString(): String = "[$property:$value]"
 }
 
 
@@ -71,28 +34,74 @@ private const val BLOCK_END = "}"
 private const val LINE_SEPARATOR = ";"
 private const val DECLARATION_SEPARATOR = ":"
 
-private tailrec fun splitToBlocks(tokens: List<String>,
-                                  blockList: List<StyleBlock> = emptyList()): List<StyleBlock> {
-    // Get first block end (Use block ends as indicators)
-    val firstBlockEnd = tokens.indexOf(BLOCK_END)
 
-    // If there is no block left, end the recursion and return the finished the block list
-    return if (firstBlockEnd == -1)
-        blockList
-    else
-        splitToBlocks(
-            tokens.subList(firstBlockEnd+1, tokens.size),
-            blockList + StyleBlock.fromTokens(tokens[0..firstBlockEnd])
-        )
+private inline class Tokens(val value: List<String>) : List<String> {
+
+    companion object {
+        private tailrec fun splitToBlocks0(tokens: Tokens, declarationBlockList: List<DeclarationBlock> = emptyList()): List<DeclarationBlock> {
+            // Get first block end (Use block ends as indicators)
+            val firstBlockEnd = tokens.indexOf(BLOCK_END)
+
+            // If there is no block left, end the recursion and return the finished the block list
+            return if (firstBlockEnd == -1)
+                declarationBlockList
+            else
+                splitToBlocks0(
+                    Tokens(tokens.subList(firstBlockEnd+1, tokens.size)),
+                    declarationBlockList + Tokens(tokens[0..firstBlockEnd]).toBlock()
+                )
+        }
+    }
+
+    fun clean(): Tokens = Tokens(value.filter { t -> t.isNotBlank() })
+
+    fun splitToLines(): List<Line> = value.split(LINE_SEPARATOR)
+
+    fun toBlock(): DeclarationBlock {
+        val blockStartIndex = this.indexOf(BLOCK_START)
+        val blockEndIndex = this.lastIndexOf(BLOCK_END)
+
+        val selectors = this.subList(0, blockStartIndex)
+        val lines = Tokens(this.subList(blockStartIndex+1, blockEndIndex)).splitToLines()
+
+        return DeclarationBlock(selectors, lines.map { it.toDeclaration() })
+    }
+
+    fun splitToBlocks() = splitToBlocks0(this)
+
+
+    override val size: Int get() = value.size
+
+    override fun isEmpty(): Boolean = value.isEmpty()
+
+    override fun contains(element: String): Boolean = value.contains(element)
+
+    override fun containsAll(elements: Collection<String>): Boolean = value.containsAll(elements)
+
+    override fun get(index: Int): String = value[index]
+
+    override fun subList(fromIndex: Int, toIndex: Int): List<String> = value.subList(fromIndex, toIndex)
+
+    override fun indexOf(element: String): Int = value.indexOf(element)
+
+    override fun lastIndexOf(element: String): Int = value.lastIndexOf(element)
+
+    override fun iterator(): Iterator<String> = value.iterator()
+
+    override fun listIterator(): ListIterator<String> = value.listIterator()
+
+    override fun listIterator(index: Int): ListIterator<String> = value.listIterator(index)
+
 }
 
-private fun splitToLines(tokens: List<String>): List<List<String>> = tokens.split(LINE_SEPARATOR)
 
-private fun toDeclaration(line: List<String>): StyleDeclaration {
-    if (line.isEmpty())
+private typealias Line = List<String>
+
+private fun Line.toDeclaration(): Declaration {
+    if (this.isEmpty())
         throw IllegalArgumentException("Declaration line is empty!")
 
-    val declarationParts = line.split(DECLARATION_SEPARATOR)
+    val declarationParts = this.split(DECLARATION_SEPARATOR)
 
     when {
         declarationParts.size > 2 -> throw ParserException("Too many colons: $declarationParts!")
@@ -100,13 +109,14 @@ private fun toDeclaration(line: List<String>): StyleDeclaration {
         else -> {
             // Check declaration parts size (must not exceed 1)
             declarationParts.forEach { if (it.size > 1) ParserException("Number of declaration parts exceeds 1!") }
-            return StyleDeclaration(
+            return Declaration(
                 declarationParts[0],
                 declarationParts[1]
             )
         }
     }
 }
+
 
 // TODO Move to util
 private fun List<String>.foldToString(): String = this.fold("") {
